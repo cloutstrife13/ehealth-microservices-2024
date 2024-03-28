d70f44e3e202e73b411fc3bb7c3da771a23b3e5b
Andy Le <andyle13@outlook.com>
2024-03-28 21:06:20 +0100

    Migrated from Gin to Iris

--- services/user-service/main.go
+++ services/user-service/main.go
@@ -1,30 +1,36 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/kataras/iris/v12"
 )
 
-type user struct {
-	ID            string `json:"id"`
-	FirstName     string `json:"firstName"`
-	LastName      string `json:"lastName"`
-	DateOfBirth   string `json:"dateOfBirth"`
-	InsuranceCode string `json:"insuranceCode"`
+type User struct {
+	ID          string `json:"id"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	DateOfBirth string `json:"dateOfBirth"`
+	// InsuranceCode string `json:"insuranceCode"`
 }
 
-var users = []user{
-	{ID: "1", FirstName: "Ada", LastName: "Lovelace", DateOfBirth: "10/12/1815", InsuranceCode: "K435834234"},
-	{ID: "2", FirstName: "Lightning", LastName: "Quirks", DateOfBirth: "02/06/1955", InsuranceCode: "K453656452"},
+var users = []User{
+	{ID: "1", FirstName: "Ada", LastName: "Lovelace", DateOfBirth: "10/12/1815"},
+	{ID: "2", FirstName: "Lightning", LastName: "Quirks", DateOfBirth: "02/06/1955"},
 }
 
-func getUsers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, users)
+func getUsers(ctx iris.Context) {
+	ctx.JSON(users)
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/users", getUsers)
-	router.Run(":8181")
+	app := iris.New()
+
+	usersAPI := app.Party("/users")
+	{
+		usersAPI.Use(iris.Compression)
+
+		// GET: http://localhost:8080/books
+		usersAPI.Get("/", getUsers)
+	}
+
+	app.Listen(":8181")
 }
