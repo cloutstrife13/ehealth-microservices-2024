c24e15d72d5d58eb76299177c53c1908202d6d68
Andy Le <andyle@Andys-MacBook-Air.local>
2024-03-28 16:48:59 +0100

    Initial commit for go repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cloutstrife13/ehealth-microservices
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	if err := http.ListenAndServe(":8181", nil); err != nil {
+		log.Fatal("server not started")
+	}
+}
+
+func helloHandler(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		log.Println(("GET request received, testing hot reload"))
+		name := r.URL.Query().Get("name")
+		rw.Write([]byte(fmt.Sprintf("hi %v\n", name)))
+	}
+}
