86f0df7cc8aa4218c0580f98711ee4aa19978c35
Andy Le <andyle13@outlook.com>
2024-04-26 03:54:01 +0200

    Add preliminary ingress

--- services/patient-service/patients/service.go
+++ services/patient-service/patients/service.go
@@ -3,7 +3,7 @@ package patients
 import "github.com/kataras/iris/v12"
 
 var patients = []Patient{
-	{ID: "1", FirstName: "Ada", LastName: "Lovelace", DateOfBirth: "10/12/1815"},
+	{ID: "1", FirstName: "Adana", LastName: "Lovelace", DateOfBirth: "10/12/1815"},
 	{ID: "2", FirstName: "Lightning", LastName: "Quirks", DateOfBirth: "02/06/1955"},
 }
 
